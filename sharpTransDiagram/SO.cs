b09ce127bd703ca101c7f63ba5af4f6e74ffd759
Hadimaamar <nbib99@gmail.com>
2022-01-12 09:02:10 +0200

    fixes vendor

---
+++ sharpTransDiagram/SO.cs
@@ -0,0 +1,38 @@
+﻿using WebApp.Domain.Models;
+using WebApp.Domain.Models.Transactions;
+
+namespace sharpTransDiagram
+{
+    public class SO : CompoundTransaction
+    {
+        public int HubId { get; set; }
+
+        public void CreateTransForItem(int itemId, int Qty, double price)
+        {
+            StockHubTrans sht1 = new StockHubTrans(Constants.OnSo)
+            { Id = 1, HubId = this.HubId, Direction = true, TargetId = itemId, Quantity = Qty, Price = price, TheDummy = this.TheDummy };
+            //StockHubTrans sht2 = new StockHubTrans(Constants.OnHand)
+            //{ Id = 2, Direction = true, HubId = this.HubId, TargetId = itemId, Quantity = Qty, Price = price, TheDummy = this.TheDummy };
+            this.Total += sht1.GetAmount();
+
+            this.LeafTransList.Add(sht1);
+            //this.leafTransList.Add(sht2);
+        }
+
+        public void CreateAccountTransaction()
+        {
+            AccountTrans act1 = new AccountTrans(Constants.Customer, Constants.OnSo) { Id = 1, Direction = true, TargetId = this.TargetId, Quantity = this.Total, TheDummy = this.TheDummy };
+            LeafTransList.Add(act1);
+        }
+
+        public override bool Post()
+        {
+            this.LeafTransList.ForEach(aLeaf => aLeaf.Post());
+            return true;
+        }
+
+        //public void Ship()
+        //{
+        //}
+    }
+}
---
+++ sharpTransDiagram/Vendor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Domain.Models;
+
+namespace sharpTransDiagram
+{
+    public partial class Vendor : Target
+    {
+        public int Id { get; set; }
+        public double OnPO { get; set; } = 0;
+        public double Balance { get; set; } = 0;
+    }
+
+    public partial class Vendor : Target
+    {
+        public override int GetTargetId()
+        {
+            return this.Id;
+        }
+
+        internal override bool Update(double quantity, string attribute)
+        {
+            if (string.IsNullOrWhiteSpace(attribute))
+            {
+                throw new ArgumentException($"'{nameof(attribute)}' cannot be null or whitespace.", nameof(attribute));
+            }
+
+            if (this.GetType().GetProperty(attribute) != null)
+            {
+                double value = (double)this.GetType().GetProperty(attribute).GetValue(this);
+
+                this.GetType().GetProperty(attribute).SetValue(this, value + quantity);
+
+                Console.WriteLine("Vendor (" + Id + ") : " + this.GetType().GetProperty(attribute).Name + " updated " + value + " -> " + this.GetType().GetProperty(attribute).GetValue(this).ToString() + "\n");
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
